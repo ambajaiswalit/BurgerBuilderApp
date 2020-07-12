cf7f69a55b2b09f4578e1fe543501813773cd5bb
Amba Jaiswal <Ammyjais95@gmail.com>
2020-07-12 11:48:02 +0530

    Expected an assingment or function call instead saw an expression error

--- src/components/Burger/Burger.js
+++ src/components/Burger/Burger.js
@@ -1,23 +1,34 @@
 import React from 'react';
- import classes from './Burger.module.css';
- import BurgerIngredient from './BurgerIngredient';
+import classes from './Burger.module.css';
+import BurgerIngredient from './BurgerIngredient/BurgerIngredient';
 
 const burger = (props) => {
-    //This is for changeing this object into arra
-    
-    const transforedIngrediants = Object.keys(props.Ingredients)
-    .map(igKey => {
-        return [...Array(props.ingredients[igKey])].map((_,i) => {
-            return <BurgerIngredient key = {igKey+i} type = {igKey} />;
-        });
-    }); 
+    //This is for changeing this object into array
+
+    let transformedIngredients = Object.keys(props.ingredients)
+        .map(igKey => {
+            return [...Array(props.ingredients[igKey])].map((_, i) => {
+                return <BurgerIngredient key={igKey + i} type={igKey} />;
+            });
+        })
+
+        .reduce((arr, el) => {
+            return arr.concat(el)
+        }, []);
+
+    if (transformedIngredients.length === 0) {
+        transformedIngredients = <p>Please start adding ingredients !</p>
+    }
+
+    console.log(transformedIngredients);
+
     return (
         <div className={classes.Burger}>
-           <BurgerIngredient type="bread-top"/>
-           {transforedIngrediants}
-           <BurgerIngredient type="bread-bottom"/>
+            <BurgerIngredient type="bread-top" />
+            {transformedIngredients}
+            <BurgerIngredient type="bread-bottom" />
         </div>
     );
- };
+};
 
- export default burger;
+export default burger;
